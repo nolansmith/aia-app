54da8be35f3984ac39dc0b2a52b20de86948dc00
nolansmith <nolanwsmith@gmail.com>
2023-09-01 03:46:59 -0500

    init update

--- scripts/init.sh
+++ scripts/init.sh
@@ -5,5 +5,6 @@ echo "--    PULLING IMAGES FROM DOCKER     --"
 echo "---------------------------------------"
 
 docker pull nginx:latest
+docker pull python:3.9
 docker pull node:14-alpine
 docker pull postgres:13-alpine
